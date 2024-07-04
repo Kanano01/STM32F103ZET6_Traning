dea4ddf14a46ff648acfb07edfa7a8ed08843c0e
kanano01 <weixianglongaz@gmail.com>
2024-07-05 01:18:25 +0800

    第一次上传，led点灯版本

---
+++ kanano01_led/Drivers/BSP/led.c
@@ -0,0 +1,27 @@
+#include "./led.h"
+//########################################
+//1.enable clock
+//2. config setting
+//3.enable GPIO
+//##########################################
+
+char led_init(void)
+{
+	 __HAL_RCC_GPIOE_CLK_ENABLE();//enable gpioe
+	 __HAL_RCC_GPIOB_CLK_ENABLE();
+		GPIO_InitTypeDef GPIOPP;
+	
+			GPIOPP.Mode	=	 GPIO_MODE_OUTPUT_PP;
+			GPIOPP.Pin	=	GPIO_PIN_5;
+			GPIOPP.Pull	=	GPIO_PULLDOWN;
+			GPIOPP.Speed	=	GPIO_SPEED_FREQ_HIGH;
+		
+		
+
+	//void HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
+	HAL_GPIO_Init(GPIOE,&GPIOPP);
+	HAL_GPIO_Init(GPIOB,&GPIOPP);
+	
+		return 1;
+	
+}
